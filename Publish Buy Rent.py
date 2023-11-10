c0d678d67743700fb09b0687371645462b2a3c58
Phantom <82107832+phantom-712@users.noreply.github.com>
2023-11-10 09:28:41 +0530

    Update Publish Buy Rent.py

--- Publish Buy Rent.py
+++ Publish Buy Rent.py
@@ -1,28 +1,28 @@
 Nom=int(input("Enter 1. if you want to publish your book/s, 2. if you want to Buy a book, 3. if you want to rent a book"))
-
+'''initialise the slot_no var'''
 def publish():
-    n=int(input("Num of books you want to publish"))
+    n=int(input("Num of books you want to publish: "))
     for i in range(n):
         slot_no+=75+1
-        name=input('Enter name of book')
-        author=input('Enter name of the author of book')
-        genre=input('Enter genre of book')
-        agelimit=input('Enter age limit')
+        name=input('Enter name of book: ')
+        author=input('Enter name of the author of book: ')
+        genre=input('Enter genre of book: ')
+        agelimit=input('Enter age limit: ')
         print('\n')
         ins="insert into booklist value({},'{}','{}','{}','{}')".format(slot_no,name,author,genre,agelimit)
         cr.execute(ins)
         con.commit()
 c,c1=0,0        
 def BuyOrRent():
-    M=int(input("Enter:",'\n','1 for Search by Latest in Collection, 2 for Search by Popular this month, 3 for Search by Genre')
+    M=int(input("Enter:",'\n','1 for Search by Latest in Collection, 2 for Search by Popular this month, 3 for Search by Genre: ')
     def Latest():
         c=0
         print("4 books have been added recently:",'\n',"JOURNEY TO THE CENTRE OF THE EARTH",'\n',"THE HARRY POTTER SERIES",'\n',"THE PILLARS OF THE EARTH (SERIES)",'\n','GHOST WORLD')
         Bk=''
-        Bk=input("Enter the name of the book, if interested to get further details, else press NO")
+        Bk=input("Enter the name of the book, if interested to get further details, else press NO: ")
         if Bk="NO":
             c+=1
-            print("We see that you are not interested in these books, please select from other options")
+            print("We see that you are not interested in these books, please select from other options: ")
             break
         else:
             cr.execute("select * from library where name=Bk")
@@ -44,10 +44,10 @@ def BuyOrRent():
         print("IN SEARCH OF LOST TIME")
         print("LEAGUE OF LEGENDS")
         Bk=''
-        Bk=input("Enter the name of the book, if interested to get further details, else press NO")
+        Bk=input("Enter the name of the book, if interested to get further details, else press NO: ")
         if Bk ="NO":
             c+=1
-            print("We see that you are not interested in these books, please select from other options")
+            print("We see that you are not interested in these books, please select from other options: ")
             break
         else:
             cr.execute("select * from library where name=Bk")
@@ -68,12 +68,12 @@ def BuyOrRent():
         for i in res:
             print(i)
         
-        Bk=input("Enter the name of the book you want to buy, if not press NO")
+        Bk=input("Enter the name of the book you want to buy, if not press NO: ")
         if  Bk=='NO':
-            gg=input("If u want to read a different genre, enter YES, else enter NO again")
+            gg=input("If u want to read a different genre, enter YES, else enter NO again: ")
             if gg="NO":
                 c+=1
-                print("We see that you are not interested in these books, please select from other options")
+                print("We see that you are not interested in these books, please select from other options: ")
                 break
             else:
                 c1+=1
