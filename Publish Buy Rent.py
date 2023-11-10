0b9b9ddee8cf11cbfe86b137f64bef6f1579fea1
Phantom <82107832+phantom-712@users.noreply.github.com>
2023-11-11 00:27:22 +0530

    Update Publish Buy Rent.py

--- Publish Buy Rent.py
+++ Publish Buy Rent.py
@@ -1,5 +1,7 @@
 #Put this whole program under def homepage()
-Nom=int(input("Enter:",'\n'," 1. if you want to publish your book/s\n\n2. if you want to Buy a book,\n\n3. if you want to rent a book\n\n4. Exit"))
+print("Enter:",'\n'," 1. if you want to publish your book(s)\n\n2. if you want to Buy a book,\n\n3. if you want to rent a book\n\n4. Exit\n")
+Nom=int(input())
+dead_end=0
 def publish():
     n=int(input("Num of books you want to publish: "))
     slot_no=75
@@ -16,13 +18,14 @@ def publish():
 c,c1=0,0        
 def BuyOrRent():
     dead_end=0
-    M=int(input("Enter:",'\n','1. To see Latest in Collection, 2. To see Popular this month, 3 To Search a book by Genre: ')
+    print("Enter:",'\n','1. To see Latest in Collection, 2. To see Popular this month, 3 To Search a book by Genre: ')
+    M=int(input())
     def Latest():
         c=0
         print("4 books have been added recently:",'\n',"1. JOURNEY TO THE CENTRE OF THE EARTH",'\n',"2. THE HARRY POTTER SERIES",'\n',"3. THE PILLARS OF THE EARTH (SERIES)",'\n','4. GHOST WORLD')
         Bk=''
         Bk=input("Enter the name of the book, if interested to get further details, else press NO: ")
-        if Bk="NO" or Bk='no':
+        if Bk=="NO" or Bk=='no':
             c+=1
             print("We see that you are not interested in these books, please select from other options: ") #dead end direct to buy or rent menu 
             break
@@ -48,7 +51,7 @@ def BuyOrRent():
         print("4. LEAGUE OF LEGENDS")
         Bk=''
         Bk=input("Enter the name of the book, if interested to get further details, else press NO: ")
-        if Bk ="NO" or Bk='no':
+        if Bk =="NO" or Bk=='no':
             c+=1
             print("We see that you are not interested in these books, please select from other options: ")#dead end direct to buy or rent menu 
             break
@@ -63,9 +66,10 @@ def BuyOrRent():
                 print("To rent the book-",Bk,'/t',"You have to pay ₹100 per week")
             return(Bk)
     def Genre():
-        c=0,c1=0
+        c,c1=0,0
         Bk=''
-        g=input("Enter the genre you want to read:","\n","1)FICTION",'/n',"2)SCIENCE FICTION",'/n',"3)MYSTERY",'/n',"4)NON FICTION",'/n',"5)ROMANCE",'/n',"6)HORROR",'/n','7)AUTOBIOGRAHY','/n','8)GRAPHIC NOVEL','/n',"9)FAIRY TALES",'/n',"10)DRAMA",'/n')
+        print("Enter the genre you want to read:","\n","1)FICTION",'/n',"2)SCIENCE FICTION",'/n',"3)MYSTERY",'/n',"4)NON FICTION",'/n',"5)ROMANCE",'/n',"6)HORROR",'/n','7)AUTOBIOGRAHY','/n','8)GRAPHIC NOVEL','/n',"9)FAIRY TALES",'/n',"10)DRAMA",'/n')
+        g=input()
         cr.execute("select * from booklist where genre=g")# will be refers to the booklist table here not library table.lib is database
         res=cr.fetchall()
         no=1
@@ -76,7 +80,7 @@ def BuyOrRent():
         Bk=input("Enter the name of the book you want to buy, if not press NO: ")
         if  Bk=='NO':
             gg=input("If u want to read a different genre, enter YES, else enter NO again: ")
-            if gg="NO":
+            if gg=="NO":
                 c+=1
                 print("We see that you are not interested in these books, please select from other options: ")#same mistake as in 26
                 break
@@ -102,8 +106,7 @@ def BuyOrRent():
         Genre()
         
  #c au c1 ra purpose plz explain.   
-if dead_end!=0:
-    BuyOrRent()
+
 if Nom==1:
     publish()
     print("Your book(s) have been successfully published.")
@@ -117,8 +120,12 @@ if Nom==3:
     #direct to checkout fn/end fn
 if Nom==4:
     print('Thank you for visitng UNREAL LIBRARY.')    
+if dead_end!=0:
+    BuyOrRent()
     
-    
+    '''check for any further errors/
+    +working check karihabani karana tables create hin heini
+    last ku sabu compile karila pare check haba'''
         
         
         
