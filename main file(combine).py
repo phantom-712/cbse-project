3123416d32bd60da015db61fd96e1884229c92b5
Phantom <82107832+phantom-712@users.noreply.github.com>
2023-11-13 19:03:54 +0530

    Update main file(combine).py

--- main file(combine).py
+++ main file(combine).py
@@ -1,8 +1,8 @@
 '''
 Its the base of the home page part which mycursoreates the basic tables. and braches out to others.
 '''
-import mysql.connector
 import sys
+import mysql.connector
 passw=input("Enter you mysql password: ")
 mydb = mysql.connector.connect(host='localhost', password=passw, user='root') 
 
@@ -64,9 +64,9 @@ def homepage():
         b30 = [30, "RUNNER", "TRACY CLARK", "MYSTERY", "UA"]
         b31 = [31, "REBECCA", "DAPHNE DU MAURIER", "MYSTERY", "PG-13"]
         b32 = [32, "GONE GIRL", "GILLIAN FLYNN", "MYSTERY", "R"]
-        b33 = [33, "IN THE WOODS (Series)", "TANA FRENCH", "MYSTERY", "PG-13"]
+        b33 = [33, "IN THE WOODS", "TANA FRENCH", "MYSTERY", "PG-13"]
         b34 = [34, "A STUDY IN SCARLET WOMEN", "SHERRY THOMAS", "MYSTERY", "PG-13"]
-        b35 = [35, "ONE OF THE MONKEY (Series)", "JANET EVANOVICH", "MYSTERY", "PG-UA"]
+        b35 = [35, "ONE OF THE MONKEY", "JANET EVANOVICH", "MYSTERY", "PG-UA"]
         b36 = [36, "THE SIXTH EXTNCTION", "ELIZABETH KOLBERT", "NON FICTION", "UA"]
         b37 = [37, "THE YEAR OF MAGICAL THINKING", "JOAN DIDION", "NON FICTION", "Not Given"]
         b38 = [38, "BIRTHDAY LETTERS", "TED HUGHES", "NON FICTION", "R"]
@@ -77,49 +77,53 @@ def homepage():
         b43 = [43, "THE WASTE LAND", "TS ELIOT", "NON FICTION", "PG-13"]
         b44 = [44, "NO LOGO", "NAOMI KLEIN", "NON FICTION", "PG-15"]
         b45 = [45, "DREAM FROM MY FATHER", "BARACK OBAMA", "NON FICTION", "PG-13"]
-        b46 = [46, "THE RIGHT STUFF (Series)", "TOM WOLFE", "NON FICTION", "UA"]
-        b47 = [47, "INTO THE WILD (Series)", "LON KRAKAUER", "NON FICTION", "PG-13"]
-        b48 = [48, "FREAKONOMICS (Series)", "STEVEN D. LEVITT AND STEPHEN J. DUBNER", "NON FICTION", "PG-13"]
+        b46 = [46, "THE RIGHT STUFF", "TOM WOLFE", "NON FICTION", "UA"]
+        b47 = [47, "INTO THE WILD", "LON KRAKAUER", "NON FICTION", "PG-13"]
+        b48 = [48, "FREAKONOMICS", "STEVEN D. LEVITT AND STEPHEN J. DUBNER", "NON FICTION", "PG-13"]
         b49 = [49, "MISTRESS", "AMANDA QUICK", "ROMANCE", "PG-13"]
         b50 = [50, "RAVISHED", "AMANDA QUICK", "ROMANCE", "PG-13"]
         b51 = [51, "DANGEROUS", "AMANDA QUICK", "ROMANCE", "PG-13"]
-        b52 = [52, "THE RAVENELS (Series)", "LISA KLEYPAS", "ROMANCE", "R"]
+        b52 = [52, "THE RAVENELS", "LISA KLEYPAS", "ROMANCE", "R"]
         b53 = [53, "NIGHT SHIFT", "STEPHEN KING", "HORROR", "UA"]
         b54 = [54, "THE OCTOBER COUNTRY", "RAY BRADBURY, JOE MUGNAINA", "HORROR", "Not Rated"]
         b55 = [55, "THE BLOOD CHAMBER AND OTHER STORIES", "ANGELA CARTER", "HORROR", "R"]
-        b56 = [56, "BOOKS OF BLOOD (Series)", "CLIVE BARKER", "HORROR", "PG-13"]
+        b56 = [56, "BOOKS OF BLOOD", "CLIVE BARKER", "HORROR", "PG-13"]
         b57 = [57, "THE DIARY OF A YOUNG GIRL", "ANNE FRANK", "AUTOBIOGRAPHY", "UA"]
         b58 = [58, "BECOMING", "MICHELLE OBAMA", "AUTOBIOGRAPHY", "PG-13"]
         b59 = [59, "LONG WALK TO FREEDOM", "NELSON MANDELA", "AUTOBIOGRAPHY", "PG-13"]
-        b60 = [60, "CHRONICLES (Series)", "BOB DYLAN", "AUTOBIOGRAPHY", "PG-13"]
+        b60 = [60, "CHRONICLES", "BOB DYLAN", "AUTOBIOGRAPHY", "PG-13"]
         b61 = [61, "WATCHMEN", "ALAN MOORE , DAVE GIBBONMS , JOHN HIGGINS", "GRAPHIC NOVEL", "PG-13"]
         b62 = [62, "BATMAN:THE KILLING JOKE", "ALAN MOORE , BRIAN BOLLAND , TIM SALE", "GRAPHIC NOVEL", "R"]
         b63 = [63, "GHOST WORLD", "DANIEL CLOWES", "GRAPHIC NOVEL", "PG-13"]
-        b64 = [64, "THE AVENGERS (Series)", "STAN LEE", "GRAPHIC NOVEL", "UA"]
+        b64 = [64, "THE AVENGERS", "STAN LEE", "GRAPHIC NOVEL", "UA"]
         b65 = [65, "GOOD OMEN", "NEIL GAIMAN", "FAIRY TALE", "R"]
         b66 = [66, "HARRIET HALL AND THE MIRACLE CURE", "SONIA GARRETT", "FAIRY TALE", "UA"]
         b67 = [67, "LEAGUE OF LEGENDS", "RIOT GAMES", "FAIRY TALE", "PG-13"]
-        b68 = [68, "THE QUEEN OF NOTHING (Series)", "HOLLY BLACK", "FAIRY TALE", "PG-13"]
-        b69 = [69, "MAGIC KINGDOM (Series)", "ZENITH PUBLISHING", "FAIRY TALE", "UA"]
+        b68 = [68, "THE QUEEN OF NOTHING", "HOLLY BLACK", "FAIRY TALE", "PG-13"]
+        b69 = [69, "MAGIC KINGDOM", "ZENITH PUBLISHING", "FAIRY TALE", "UA"]
         b70 = [70, "HAMLET", "WILLIAM SHAKESPEARE", "DRAMA", "UA"]
         b71 = [71, "ROMEO AND JULIET", "WILLIAM SHAKESPEARE , DR. BARBARA A. MOWAT , PAUL WERSTINE", "DRAMA", "UA"]
         b72 = [72, "THE FAULT IN OUR STARS", "JOHN GREEN", "DRAMA", "PG-13"]
         b73 = [73, "OTHELLO", "WILLIAM SHAKESPEARE", "DRAMA", "UA"]
-        b74 = [74, "TWILIGHT (Series)", "STEPHENIE MEYER", "DRAMA", "PG-13"]
-        b75 = [75, "THE PILLARS OF THE EARTH (Series)", "KEN FOLLETT", "DRAMA", "R"]
+        b74 = [74, "TWILIGHT", "STEPHENIE MEYER", "DRAMA", "PG-13"]
+        b75 = [75, "THE PILLARS OF THE EARTH", "KEN FOLLETT", "DRAMA", "R"]
         l = [b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22,b23, b24, b25, b26, b27, b28, b29, b30, b31, b32, b33, b34, b35, b36, b37, b38, b39, b40, b41, b42,b43, b44, b45, b46, b47, b48, b49, b50, b51, b52, b53, b54, b55, b56, b57, b58, b59, b60, b61, b62,b63, b64, b65, b66, b67, b68, b69, b70, b71, b72, b73, b74, b75]
         for i in range(75):
-            sn = l[i][0]
+            Slot_No = l[i][0]
             Bookname = l[i][1]
-            author = l[i][2]
-            genre = l[i][3]
-            agelimit = l[i][4]
-            ins = "insert into booklist values({},'{}','{}','{}','{}')".format(sn, Bookname, author, genre, agelimit)
+            Author = l[i][2]
+            Genre = l[i][3]
+            Age_Limit = l[i][4]
+            ins = "insert into booklist value({},'{}','{}','{}','{}')".format(Slot_No, Bookname, Author, Genre, Age_Limit)
             mycursor.execute(ins)
-            mydb.commit()
-
-    print("Enter:", '\n',"1. if you want to publish your book(s)\n\n 2. if you want to Buy a book,\n\n 3. if you want to rent a book\n\n 4. Exit\n")
-    Nom = int(input())
+            mydb.commit()        
+    print("Enter:", '\n'," 1. if you want to publish your book(s)\n\n2. if you want to Buy a book,\n\n3. if you want to rent a book\n\n4. Exit\n")
+    Nom = 6
+    while Nom not in range(1,5):
+            Nom = int(input())
+            if Nom in range(1,4):
+                            break
+            print("invalid input. Please enter a valid number from 1 to 4: ")            
     dead_end = 0
 
     def publish():
@@ -140,29 +144,38 @@ def homepage():
 
     def BuyOrRent():
             dead_end = 0
-            print('Enter:\n1. To see Latest in Collection \n2. To see Popular this month  \n3 To Search a book by Genre: ')
-            M = int(input())
-
+            print("Enter:", '\n','1. To see Latest in Collection, 2. To see Popular this month, 3. To Search a book by Genre, 4. to view all the books')
+            M=6            
+            while M not in range(1,5):
+                    M = int(input())
+                    if M in range(1,5):
+                            break
+                    print("invalid input. Please enter a valid number from 1 to 5 ")
+                    
+            
             def Latest():
                 c = 0
-                print("4 books have been added recently:", '\n', "1. JOURNEY TO THE CENTRE OF THE EARTH", '\n',"2. THE HARRY POTTER SERIES", '\n', "3. THE PILLARS OF THE EARTH (SERIES)", '\n','4. GHOST WORLD')
+                print("4 books have been added recently:", '\n', "1. JOURNEY TO THE CENTRE OF THE EARTH", '\n',"2. THE HARRY POTTER SERIES", '\n', "3. THE PILLARS OF THE EARTH", '\n','4. GHOST WORLD')
                 Bk = ''
-                Bk = input("Enter the bookname you want to purchase to get further details, else press NO: ")
-                if Bk == "NO" or Bk == 'no':
-                    c += 1
-                    print("We see that you are not interested in these books, please select from other options: ")
-                    return 0
-
-                else:
-                    mycursor.execute("select * from booklist where Bookname=%s",(Bk.upper(),))
-                    for i in mycursor:
-                        print(i)
-                        break
-                    if Nom == 2:
-                        print("To buy the book-", Bk, '\t', "You have to pay ₹1000")
-                    if Nom == 3:
-                        print("To rent the book-", Bk, '\t', "You have to pay ₹100 per week")
-                    return (Bk)
+                while True:
+                        Bk = input("Enter the name of the book(in caps), if interested to get further details, else press NO: ")
+                
+                        if Bk == "NO" or Bk == 'no':
+                                    c += 1
+                                    print("We see that you are not interested in these books, please select from other options: ")
+                                    return 0
+                        if Bk not in("JOURNEY TO THE CENTRE OF THE EARTH","THE HARRY POTTER SERIES","THE PILLARS OF THE EARTH","GHOST WORLD"):                        
+                                print("Please try again") 
+                                continue
+                        else:                                
+                                mycursor.execute("select * from booklist where Bookname=%s",(Bk,))
+                                for i in mycursor:
+                                        print(i)
+                                        if Nom == 2:
+                                                print("To buy the book-", Bk, '/t', "You have to pay ₹1000")
+                                        if Nom == 3:
+                                                print("To rent the book-", Bk, '/t', "You have to pay ₹100 per week")
+                                        return (Bk.upper())                                                                
 
             def Popular():
                 c = 0
@@ -172,68 +185,102 @@ def homepage():
                 print("3. IN SEARCH OF LOST TIME")
                 print("4. LEAGUE OF LEGENDS")
                 Bk = ''
-                Bk = input("Enter the name of the book, if interested to get further details, else press NO: ")
-                if Bk == "NO" or Bk == 'no':
-                    c += 1
-                    print("We see that you are not interested in these books, please select from other options: ") 
-                    return 0
-                else:
-                    mycursor.execute("select * from booklist where Bookname=%s",(Bk.upper(),))
-                    for i in mycursor:
-                        print(i)
-                        break
-                    if Nom == 2:
-                        print("To buy the book-", Bk, '\t', "You have to pay ₹1000")
-                    if Nom == 3:
-                        print("To rent the book-", Bk, '\t', "You have to pay ₹100 per week")
-                    return (Bk)
-
+                while True:
+                        
+                        Bk = input("Enter the name of the book(in caps), if interested to get further details, else press NO: ")
+                        if Bk == "NO" or Bk == 'no':
+                            c += 1
+                            print("We see that you are not interested in these books, please select from other options: ") 
+                            return 0
+                        if Bk not in("MY SISTER, THE SERIAL KILLER","FAHRENHEIT 451","IN SEARCH OF LOST TIME","LEAGUE OF LEGENDS"):
+                                print("Please try again")
+                                continue
+                        else:
+                                mycursor.execute("select * from booklist where Bookname=%s",(Bk,))
+                                for i in mycursor:
+                                        print(i)                                
+                                if Nom == 2:
+                                        print("To buy the book-", Bk, '/t', "You have to pay ₹1000")
+                                if Nom == 3:
+                                        print("To rent the book-", Bk, '/t', "You have to pay ₹100 per week")
+                                return (Bk.upper())
             def Genre():
                 c, c1 = 0, 0
                 Bk = ''
-                print("Enter the genre you want to read:", "\n", "1)FICTION", '\n', "2)SCIENCE FICTION", '\n', "3)MYSTERY",'\n', "4)NON FICTION", '\n', "5)ROMANCE", '\n', "6)HORROR", '\n', '7)AUTOBIOGRAHY', '\n','8)GRAPHIC NOVEL', '\n', "9)FAIRY TALES", '\n', "10)DRAMA", '\n')
+                print("Enter(in caps) the genre you want to read:", "\n", "1)FICTION", '/n', "2)SCIENCE FICTION", '/n', "3)MYSTERY",'/n', "4)NON FICTION", '/n', "5)ROMANCE", '/n', "6)HORROR", '/n', '7)AUTOBIOGRAHY', '/n','8)GRAPHIC NOVEL', '/n', "9)FAIRY TALES", '/n', "10)DRAMA", '/n')
                 g = input()
                 mycursor.execute("select * from booklist where genre=%s",(g,)) 
                 no = 1
                 for i in mycursor:
                     print(no, i)
                     no += 1
-
-                Bk = input("Enter the name of the book you want to buy, if not press NO: ")
-                if Bk == 'NO' or Bk=='no':
-                    gg = input("If u want to read a different genre, enter YES, else enter NO again: ")
-                    if gg == "NO":
+                while True:
+                        
+                        Bk = input("Enter the name of the book you want to buy, if not press NO: ")
+                
+                        if Bk == 'NO' or Bk=="no":
+                            gg = input("If u want to read a different genre, enter YES, else enter NO again: ")
+                            if gg == "NO":
+                                c += 1
+                                print( "We see that you are not interested in these books, please select from other options: ")  
+                                return 0
+                            else:
+                                c1 += 1
+                        mycursor.execute("select Bookname from booklist ") 
+                        if (Bk,) not in mycursor:
+                                print("Please try again")
+                                continue                            
+                        else:
+                            if Nom == 2:
+                                print("To buy the book-", Bk, '/t', "You have to pay ₹1000")
+                            if Nom == 3:
+                                print("To rent the book-", Bk, '/t', "You have to pay ₹100 per week")
+                            return (Bk.upper())
+          
+            def ViewAll():
+                 c=0
+                 print("Here are all the books available in the library")
+                 mycursor.execute("Select * from Booklist")
+                 for i in mycursor:
+                     print(i)
+                 while True:
+                    Bk = input("Enter the name of the book, if interested to get further details, else press NO: ")
+                    if Bk == "NO" or Bk == 'no':                        
                         c += 1
-                        print( "We see that you are not interested in these books, please select from other options: ")  
-                            
+                        print("We see that you are not interested in these books, please select from other options: ")
+                        return 0
+                    mycursor.execute("Select Bookname from booklist")
+                    if (Bk,) not in mycursor:
+                        print("Please enter a valid book")
+                        continue
+
                     else:
-                        c1 += 1
-                    return 0        
-                            
+                        if Nom == 2:
+                                print("To buy the book-", Bk, '/t', "You have to pay ₹1000")
+                        if Nom == 3:
+                                print("To rent the book-", Bk, '/t', "You have to pay ₹100 per week")
+                                return (Bk.upper())
 
-                else:
-                    if Nom == 2:
-                        print("To buy the book-", Bk, '\t', "You have to pay ₹1000")
-                    if Nom == 3:
-                        print("To rent the book-", Bk, '\t', "You have to pay ₹100 per week")
-                    return (Bk.upper())
+            booklist_()
         
-
             if c != 0:
                 dead_end += 1
             if M == 1:
-                Latest()
+                Book_=Latest()
             elif M == 2:
-                Popular()
+                Book_=Popular()
             elif M == 3:
-                Genre()
+                Book_=Genre()
+            elif M==4:
+                Book_=ViewAll()
             if c1 != 0:
                 Genre()
+            return(Book_)
 
         # c au c1 ra purpose plz explain.
 
 
-    booklist_()    
+        
 
     if Nom == 1:
         publish()
@@ -252,14 +299,12 @@ def homepage():
     if Nom == 4:
         print('Thank you for visitng UNREAL LIBRARY.')
     else  :        
-        repeat=input("Enter A to purchase another book or P to publish a book or E to exit")
+        repeat=input("Enter A to purchase another book or P to publish a book")
         if repeat=='A' or repeat=='a':
                 BuyOrRent()
         elif repeat=='P' or repeat=='p':
                 publish()    
                 bill()
-        elif  repeat=='E' or repeat=='e':
-                sys.exit("thank you for visiting us.")  
 
         
     def bill():
@@ -328,17 +373,24 @@ def start_menu():
     time_delay()
     print("But before that we would like to know if you are an existing user or new user.")
     time_delay()    
-    ch=input("if existing user,enter the user-code or press N for new user: ")
-    if ch=="N" or ch=="n":
-        newcust=1    
-        custname=input("Welcome aboard!\nPlease enter your name: ")  
+    ch=input("if existing user,enter the user-code or press N(in caps) for new user: ")
+    mycursor.execute("select code from oc")
+    while True:
+            if ch =="N":
+                    break
+            if ch !="N" or (ch,) not in mycursor:
+                    print("Please enter N or the valid user code")
+                    ch=input()
+                    continue
+            else:
+                    break
+    if ch=="N":
+            newcust=1
+            custname=input("Welcome aboard!\nPlease enter your name: ")
     elif (ch,) in x:#means an existing user
-        time_delay()
-        print("Glad to have an existing customer back.You will be receiving a special discount at the end. Just enter the code - 'UNREAL' ")  
-    else:
-        print("Wrong choice! \n Try Again. ")
-        wrong_choice=1
-        
+                    time_delay()
+                    print("Glad to have an existing customer back.You will be receiving a special discount at the end. Just enter the code - 'UNREAL' ") 
+       
 def end_menu():#calculation  of total amount billing address discount add here.
         print("We would like to give you a special reward")
         time_delay()
@@ -371,11 +423,8 @@ def end_menu():#calculation  of total amount billing address discount add here.
 newcust=0
 bor=0#bor=2 for buying and 3 for renting
 custname=''
-wrong_choice=0
 default()
 start_menu()
-if wrong_choice!=0:
-    start_menu()
 homepage()    
 end_menu()
 
